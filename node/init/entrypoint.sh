f4891863787b3a454292cbe5dc3ae530d7434c42
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-08-05 11:25:07 -0500

    node: enable geth & ipfs logging.

--- node/init/entrypoint.sh
+++ node/init/entrypoint.sh
@@ -4,10 +4,10 @@ if [ ! -f  /data/ipfs/datastore_spec ]; then
     /usr/bin/ipfs init > /dev/null
 fi
 
-/usr/bin/ipfs daemon --migrate=true > /dev/null &
+/usr/bin/ipfs daemon --migrate=true > /data/ipfs/ipfs.log 2>&1 &
 IPFS=$!
 
-/usr/bin/geth --datadir /data/geth --ws --wsport 8545 --verbosity 0 --goerli --syncmode light > /dev/null &
+/usr/bin/geth --datadir /data/geth --ws --wsport 8545 --goerli --syncmode light > /data/geth/geth.log 2>&1 &
 GETH=$!
 
 su-exec elcaro python3 /elcaro/main.py $@
