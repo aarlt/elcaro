9f0ef9b5e248f3055882145180d6357374711342
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-08-04 19:38:35 -0500

    docker: changed to light-client mode.

--- node/init/entrypoint.sh
+++ node/init/entrypoint.sh
@@ -7,7 +7,7 @@ fi
 /usr/bin/ipfs daemon --migrate=true > /dev/null &
 IPFS=$!
 
-/usr/bin/geth --datadir /data/geth --ws --wsport 8545 --verbosity 0 --goerli > /dev/null &
+/usr/bin/geth --datadir /data/geth --ws --wsport 8545 --verbosity 0 --goerli --syncmode light > /dev/null &
 GETH=$!
 
 su-exec elcaro python3 /elcaro/main.py $@
