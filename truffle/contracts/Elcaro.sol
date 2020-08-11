05c774284f0e6c74fcd5fdf48086c73c7df65867
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-08-10 21:43:41 -0500

    contracts: initial elcaro contract.

--- truffle/contracts/Elcaro.sol
+++ truffle/contracts/Elcaro.sol
@@ -1,47 +1,62 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
 
 import "./Owned.sol";
 import "./HitchensOrderStatisticsTree.sol";
 
 contract Elcaro is Owned {
-    struct Request {
-        string scriptCID;
-        string scriptFunction;
-        string scriptFunctionInput;
-        address outputContract;
-        string outputMethod;
-        bool stdoutCID;
-        bool stderrCID;
-        uint block;
-    }
-
     // events
-    event onRequest(address node_account, uint256 request_hash);
-    event onRegister(address node_account, uint256 node_count);
-    event onUnregister(address node_account, uint256 node_count);
+    event onRequest(address indexed node_account, bytes32 indexed request_hash, bytes data);
+    event onRegister(address indexed node_account, uint256 node_count);
+    event onUnregister(address indexed node_account, uint256 node_count);
 
     using HitchensOrderStatisticsTreeLib for HitchensOrderStatisticsTreeLib.Tree;
     HitchensOrderStatisticsTreeLib.Tree tree;
 
-    mapping(uint256 => Request) requests;
     mapping(address => uint256) nodes;
+    mapping(bytes32 => uint256[]) requests;
 
     constructor() {
     }
 
     // node management
     function register() external payable returns (bool) {
-        nodes[msg.sender] += msg.value;
-        emit onRegister(tx.origin, 0);
-
+        nodes[tx.origin] += msg.value;
+        tree.insert(bytes32("0x00"), uint256(tx.origin));
+        emit onRegister(tx.origin, tree.count());
         return true;
     }
 
     function unregister() external returns (bool) {
-        nodes[msg.sender] = 0;
-        emit onUnregister(tx.origin, 0);
-
+        nodes[tx.origin] = 0;
+        tree.remove(bytes32("0x00"), uint256(tx.origin));
+        emit onUnregister(tx.origin, tree.count());
         return true;
+    }
+
+    function nodeCount() view public returns (uint256) {
+        return tree.count();
+    }
+
+    function isRegistered(uint256 _id) view public returns (bool) {
+        return tree.keyExists(bytes32("0x00"), _id);
+    }
+
+    function call(string memory _function, bytes calldata _arguments, address _contract, string memory _callback) external payable returns (bool) {
+        // ipfs://QmZrPf6xunDiwsdbPS33oxiPQoTeztmP6KkWfFPjBjdWH7/location(string)
+        bytes memory data = abi.encode(_function, _arguments, _contract, _callback, block.number, tx.origin, msg.sender);
+        bytes32 _hash = keccak256(data);
+        uint256 _nearestNode = 0; //tree.atPermil(tree.permil(uint256(_hash)));
+        requests[_hash].push(_nearestNode);
+        emit onRequest(address(_nearestNode), _hash, data);
+        return true;
+    }
+
+    function test() external payable returns (bool) {
+        return this.call(
+            "ipfs://QmZrPf6xunDiwsdbPS33oxiPQoTeztmP6KkWfFPjBjdWH7/location(string)", abi.encode(1, 2, 3, "Hello"),
+            address(this), "updateLocation(uint256,uint256)"
+        );
     }
 }
